3fe35abe0f9dc7a1a3e0bd2bcd6f434921378c6d
xclusivesmash <sbumatshiane916@gmail.com>
2025-01-13 22:47:56 +0200

    Initial commit

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+module: 1-app
+description: a simple fask application
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Babel configuration class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel()
+babel.init_app(app)
+
+
+@app.route("/")
+def index() -> str:
+    """ renders an html page to the home path.
+    """
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000)
