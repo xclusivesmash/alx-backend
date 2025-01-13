9e17c9aa16f1a66ebd5211c9a748ba2a8e8b4734
xclusivesmash <sbumatshiane916@gmail.com>
2025-01-14 00:01:27 +0200

    Initial commit

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""
+module: 5-app
+description: user login emulation.
+"""
+from flask_babel import Babel
+from typing import Union, Dict
+from flask import Flask, render_template, request, g
+
+
+class Config:
+    """Babel configuration class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel()
+babel.init_app(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict, None]:
+    """ gets user based on id.
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """routines before each request's resolution.
+    """
+    user = get_user()
+    g.user = user
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """get locale from languages attribute."""
+    locale = request.args.get('locale', '')
+    if locale in app.config["LANGUAGES"]:
+        return locale
+    if g.user and g.user['locale'] in app.config["LANGUAGES"]:
+        return g.user['locale']
+    header_locale = request.headers.get('locale', '')
+    if header_locale in app.config["LANGUAGES"]:
+        return header_locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def index() -> str:
+    """ renders an html page to the home path"""
+    return render_template('5-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000)
