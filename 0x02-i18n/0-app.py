b899ec2e4eb02ab22b445259a5e4cd292cb22105
xclusivesmash <sbumatshiane916@gmail.com>
2025-01-13 22:15:48 +0200

    Updated some code

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -10,9 +10,11 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def index() -> str:
+    """ renders an html page to the home path.
+    """
     return render_template("0-index.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
