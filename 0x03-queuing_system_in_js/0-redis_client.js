4ea8cd4c0a62e405cdcacff10aaaf6812a12a58d
xclusivesmash <sbumatshiane916@gmail.com>
2025-01-29 11:21:54 +0200

    Initial commit

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,7 @@
+/* starts a connection to redis server */
+import { createClient } from 'redis';
+
+const client = createClient();
+client.on('error', err => console.log('Redis client not connected to the server:', err));
+await client.connect();
+console.log("Redis client connected to the server");
